3d007c0fcff84fab5dcacca3480db6bc5ec52b01
kaktusss123 <PaRoLoK123>
2019-05-02 01:00:11 +0300

    удалил лишнее

--- EApteka.py
+++ EApteka.py
@@ -1,24 +1,4 @@
-from requests import Session, get
-from json import loads
-from pprint import pprint
-
-
-def search(session, query):
-    BASE = 'http://e-apteka.md/ajax/search_products.php?query='
-    server_response = loads(session.get(BASE + query).text)
-    response = 'Выбери товар из списка (0 - отмена):\n'
-    for i, n in enumerate((i['name'] for i in server_response['data']), 1):
-        response += f'{i}) {n}\n'
-    return server_response
-
-
-def dialogue():
-    s = Session()
-    temp_results = search(s, 'аспирин')
-    response = 'Выбери товар из списка (0 - отмена):\n'
-    for i, n in enumerate((i['name'] for i in temp_results), 1):
-        response += f'{i}) {n}\n'
-    return response
+from requests import get
 
 
 def parse_info(name):
